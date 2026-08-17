672be7e7b947879c6ad9abf838a8822b168772d0
youxx329 <114648353+youxx329@users.noreply.github.com>
2026-08-18 05:09:11 +0900

    chore: 프로젝트 초기 설정

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="text-display text-display- text-dur-danger-text">
+      <div className="p-10 space-y-3">
+        <p className="text-h1 font-bold">메디온 MediON 12,900원</p>
+        <p className="text-body font-normal">지르텍정 500mg · 세티리진염산염</p>
+        <p className="text-body font-medium">지르텍정 500mg · 세티리진염산염</p>
+        <p className="text-body font-semibold">지르텍정 500mg · 세티리진염산염</p>
+        <p className="text-label">일반의약품 · 10정</p>
+      </div>
+    </div>
+  );
+}
